c3be4437456f62c6d26cba2e4a36cbcfec548e5a
Abdullah Ali <162645001+MAbdullah005@users.noreply.github.com>
2024-11-01 12:45:32 -0700

    Update program.cs
    done add node and add edge  between node of graphs
      i have done this by using a dictionary

--- program.cs
+++ program.cs
@@ -13,6 +13,26 @@ namespace Graph_dsa
         }
         Dictionary<string, Node> _nodes = new Dictionary<string, Node>();
         Dictionary<Node, List<Node>> adjaencylist = new Dictionary<Node, List<Node>>();
+         public void add_node(string data)
+        {
+            Node node = new Node(data); 
+            _nodes.TryAdd(data, node);
+            adjaencylist.TryAdd(node, new List<Node>());
+        }
+        public void add_edge(string from, string to)
+        {
+            var fromnode = _nodes[from];
+            if (fromnode == null)
+            {
+                throw new Exception("from node is not parsent");
+            }
+            var tonode = _nodes[to];
+            if (tonode == null)
+            {
+                throw new Exception("to node is noy parsent");
+            }
+            adjaencylist[fromnode!].Add(tonode!);
+        }
     }
 
     public class Progarm
