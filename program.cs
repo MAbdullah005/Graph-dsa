5895c61bb952589f9adc41118fb369062c877f82
Abdullah Ali <162645001+MAbdullah005@users.noreply.github.com>
2024-11-01 04:55:49 -0700

    start graph

---
+++ program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Graph_dsa
+{
+    public class Graph
+    {
+        private class Node
+        {
+            public string lable;
+            public Node(string lable)
+            {
+                this.lable = lable;
+            }
+        }
+        Dictionary<string, Node> _nodes = new Dictionary<string, Node>();
+        Dictionary<Node, List<Node>> adjaencylist = new Dictionary<Node, List<Node>>();
+    }
+
+    public class Progarm
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
